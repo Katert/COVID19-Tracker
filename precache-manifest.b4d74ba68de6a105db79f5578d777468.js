a75ef0257bbd1ea67a37dc4b8b814f4872c56a7c
Wesley Kater <wesley.kater@icloud.com>
2020-06-13 11:49:53 +0200

    Updates

---
+++ precache-manifest.b4d74ba68de6a105db79f5578d777468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c387b493a661d30521ecf219928850",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "ecb183363f39993e502b",
+    "url": "/covid19-tracker/static/css/main.9a5af548.chunk.css"
+  },
+  {
+    "revision": "325e03bc39feb8de6e4b",
+    "url": "/covid19-tracker/static/js/2.cc7a801c.chunk.js"
+  },
+  {
+    "revision": "f3a464b094a42227540b3ef9a73a0401",
+    "url": "/covid19-tracker/static/js/2.cc7a801c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb183363f39993e502b",
+    "url": "/covid19-tracker/static/js/main.2d567636.chunk.js"
+  },
+  {
+    "revision": "65a3730e1223978cc76b",
+    "url": "/covid19-tracker/static/js/runtime-main.b58be0d5.js"
+  }
+]);
