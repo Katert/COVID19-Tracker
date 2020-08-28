029986d5c2fee3ee20f6b2d801b2fd4ffab18951
Wesley Kater <wesley.kater@icloud.com>
2020-08-28 04:34:23 +0200

    Updates

--- precache-manifest.b28c07de1b230ccdb7505ab01d3b6079.js
+++ precache-manifest.b28c07de1b230ccdb7505ab01d3b6079.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0cd5796b791f2769f7f1d25107eddff2",
+    "revision": "4f34ce42f31cc8b45a13d8fae62ba3e1",
     "url": "/covid19-tracker/index.html"
   },
   {
-    "revision": "277ba799ec5bb2cd8199",
+    "revision": "8e1726f023cd695b3cc0",
     "url": "/covid19-tracker/static/css/main.ee055f3d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19-tracker/static/js/2.28ae14a2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "277ba799ec5bb2cd8199",
-    "url": "/covid19-tracker/static/js/main.353a472f.chunk.js"
+    "revision": "8e1726f023cd695b3cc0",
+    "url": "/covid19-tracker/static/js/main.4bb63533.chunk.js"
   },
   {
     "revision": "f33b64cae628d75b3101",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-tracker/precache-manifest.ee1bcee4392671aa4941172f66bb4b27.js"
+  "/covid19-tracker/precache-manifest.b28c07de1b230ccdb7505ab01d3b6079.js"
 );
 
 self.addEventListener('message', (event) => {
